614bf02cd90fdbe609ac815701d6f05f1e21824a
dhirajkumar62 <dhirajkumaryadav831@gmail.com>
2026-08-09 03:39:11 +0530

    Fix profile and password API routes

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,16 +1,16 @@
 import API from "./api";
 
 export const updateProfile = async (userData) => {
-    const response = await API.put("/users/profile", userData);
+    const response = await API.put("/user/profile", userData);
     return response.data;
 };
 
 export const changePassword = async (passwordData) => {
-    const response = await API.put("/users/change-password", passwordData);
+    const response = await API.put("/user/change-password", passwordData);
     return response.data;
 };
 
 export const completeProfile = async (profileData) => {
-    const response = await API.put("/users/complete-profile", profileData);
+    const response = await API.put("/user/complete-profile", profileData);
     return response.data;
 };
